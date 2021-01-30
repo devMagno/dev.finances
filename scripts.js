8fd64ef65c70448fa84e6b73e8b4a8b2d5bc70bd
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-01-30 16:27:30 -0300

    Adiciona funções da seção de transações

--- scripts.js
+++ scripts.js
@@ -19,3 +19,28 @@ const modal = {
     document.querySelector('.modal-overlay').classList.remove('active')
   } 
 }
+const DOM = {
+  transactionContainer: document.querySelector('#data-table tbody'),
+  innerHTMLTransaction(transaction) {
+    const cssClass = transaction.amount > 0 ? 'income' : 'expense'
+    const amount = utils.formatCurrency(transaction.amount)
+    const html = 
+    `
+    <td class="description">${transaction.description}</td>
+    <td class="${cssClass}">${amount}</td>
+    <td class="date">${transaction.date}</td>
+    <td>
+      <img src="assets/minus.svg" alt="Remover transação">
+    </td>
+    `
+    return html
+  },
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    this.transactionContainer.appendChild(tr)
+  },
+}
+transactions.forEach((el) => {
+  DOM.addTransaction(el)
+})
