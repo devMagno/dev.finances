8bb65bc82162f23655c02b84b9fe49ec6351d532
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-01-30 11:45:21 -0300

    Move fução do modal

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const modal = {
+  open() {
+    document.querySelector('.modal-overlay').classList.add('active')
+  },
+  close() {
+    document.querySelector('.modal-overlay').classList.remove('active')
+  } 
+}
