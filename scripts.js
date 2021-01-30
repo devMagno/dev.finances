b7b07b02c516d748879e55b2499cb9ce483dd227
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-01-30 16:20:29 -0300

    Adiciona formatCurrency

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+const utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? '-' : ''
+    value = String(value).replace(/\D/g, '')
+    value = Number(value) / 100
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+    return signal + value
+  }
+}
+
 const modal = {
   open() {
     document.querySelector('.modal-overlay').classList.add('active')
